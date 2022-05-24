b7ba8e06578cd8181787a7b932784126a0b9cf65
mahsa.ama <mahsa.ama1391@gmail.com>
2022-05-24 21:25:31 +0430

    last channel support

--- scripts/main.py
+++ scripts/main.py
@@ -9,7 +9,7 @@ from monai.utils import set_determinism
 from monai.transforms import (
     Activations,
     AsChannelFirstd,
-    AsChannelLast,
+    AsChannelLastd,
     AsDiscrete,
     CenterSpatialCropd,
     Compose,
@@ -76,7 +76,7 @@ train_transform = Compose(
     [
         # load 4 Nifti images and stack them together
         LoadImaged(keys=["image", "label"]),
-        AsChannelLast(keys="image"),
+        AsChannelLastd(keys="image"),
         ConvertToMultiChannelBasedOnBratsClassesd(keys="label"),
         Spacingd(
             keys=["image", "label"],
@@ -95,7 +95,7 @@ train_transform = Compose(
 val_transform = Compose(
     [
         LoadImaged(keys=["image", "label"]),
-        AsChannelLast(keys="image"),
+        AsChannelLastd(keys="image"),
         ConvertToMultiChannelBasedOnBratsClassesd(keys="label"),
         Spacingd(
             keys=["image", "label"],
