a4f6ae75c9a9ec764b557138b20a276e09fa3125
mahsa.ama <mahsa.ama1391@gmail.com>
2022-05-24 23:07:32 +0430

    args added.

--- scripts/main.py
+++ scripts/main.py
@@ -25,28 +25,44 @@ from monai.transforms import (
 )
 from utils.utils import ConvertToMultiChannelBasedOnBratsClassesd
 import glob
+import argparse
+import time
 import nibabel as nib
 
 
+parser = argparse.ArgumentParser(description="Transformer segmentation pipeline")
+parser.add_argument("--epochs", default=5, type=int, help="max number of training epochs")
+parser.add_argument("--batch_size", default=1, type=int, help="number of batch size")
+parser.add_argument("--dataset", default=2020, type=int, help="Dataset to use")
+parser.add_argument("--val_frac", default=0.25, type=float, help="fraction of data to use as validation")
+parser.add_argument("--num_heads", default=12, type=int, help="Number of heads to use")
+parser.add_argument("--embed_dim", default=512, type=int, help="Embedding dimension")
+
+
+args = parser.parse_args()
+
 root_dir = "./"
 set_determinism(seed=0)
+device = torch.device("cuda:0")
+
+ds = args.dataset
+frac = args.val_frac
+max_epochs = args.epochs
+batch_size = args.batch_size
+num_heads = args.num_heads
+embed_dim = args.embed_dim
+
 roi_size = [128, 128, 64]
 pixdim = (1.5, 1.5, 2.0)
-cache_num = 8
-device = torch.device("cuda:0")
-num_heads = 10  # 12 normally
-embed_dim = 512
-max_epochs = 5
-val_interval = 1
+
 best_metric = -1
 best_metric_epoch = -1
+
 epoch_loss_values = []
 metric_values = []
 metric_values_tc = []
 metric_values_wt = []
 metric_values_et = []
-ds = 2020
-frac = 0.25
 
 if ds == 2020:
     data_dir = "../Dataset_BRATS_2020/Training/"
@@ -85,7 +101,7 @@ train_transform = Compose(
     [
         # load 4 Nifti images and stack them together
         LoadImaged(keys=["image", "label"]),
-        AsChannelFirstd(keys="image"),
+        AsChannelLastd(keys="image"),
         ConvertToMultiChannelBasedOnBratsClassesd(keys="label"),
         Spacingd(
             keys=["image", "label"],
@@ -104,7 +120,7 @@ train_transform = Compose(
 val_transform = Compose(
     [
         LoadImaged(keys=["image", "label"]),
-        AsChannelFirstd(keys="image"),
+        AsChannelLastd(keys="image"),
         ConvertToMultiChannelBasedOnBratsClassesd(keys="label"),
         Spacingd(
             keys=["image", "label"],
@@ -121,127 +137,121 @@ val_transform = Compose(
 train_ds = Dataset(data=train_files, transform=train_transform)
 val_ds = Dataset(data=val_files, transform=val_transform)
 
-train_loader = DataLoader(train_ds, batch_size=2, shuffle=True, num_workers=2)
-val_loader = DataLoader(val_ds, batch_size=2, shuffle=False, num_workers=2)
-
-for batch_data in train_loader:
-    inputs, labels = (
-        batch_data["image"].to(device),
-        batch_data["label"].to(device),
-    )
-    print(inputs.size(), labels.size())
-    
-# model = UNet(
-#     dimensions=3,
-#     in_channels=4,
-#     out_channels=3,
-#     channels=(16, 32, 64, 128, 256),
-#     strides=(2, 2, 2, 2),
-#     num_res_units=2,
-# ).to(device)
+train_loader = DataLoader(train_ds, batch_size=batch_size, shuffle=True, num_workers=2)
+val_loader = DataLoader(val_ds, batch_size=batch_size, shuffle=False, num_workers=2)
 
 
-# loss_function = DiceCELoss(to_onehot_y=False, sigmoid=True)
-# optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4, weight_decay=1e-5)
-
-# torch.cuda.empty_cache()
-
-# for epoch in range(max_epochs):
-#     print("-" * 10)
-#     print(f"epoch {epoch + 1}/{max_epochs}")
-#     model.train()
-#     epoch_loss = 0
-#     step = 0
-#     for batch_data in train_loader:
-#         step += 1
-#         inputs, labels = (
-#             batch_data["image"].to(device),
-#             batch_data["label"].to(device),
-#         )
-#         print(inputs.size(), labels.size())
-#         optimizer.zero_grad()
-#         outputs = model(inputs)
-
-#         loss = loss_function(outputs, labels)
-#         loss.backward()
-#         optimizer.step()
-#         epoch_loss += loss.item()
-
-#     epoch_loss /= step
-#     epoch_loss_values.append(epoch_loss)
-#     print(f"epoch {epoch + 1} average loss: {epoch_loss:.4f}")
-
-#     if (epoch + 1) % val_interval == 0:
-#         model.eval()
-#         with torch.no_grad():
-#             dice_metric = DiceMetric(include_background=True, reduction="mean")
-#             post_trans = Compose(
-#                 [Activations(sigmoid=True), AsDiscrete(threshold_values=True)]
-#             )
-#             metric_sum = metric_sum_tc = metric_sum_wt = metric_sum_et = 0.0
-#             metric_count = metric_count_tc = metric_count_wt = metric_count_et = 0
-#             for val_data in val_loader:
-#                 val_inputs, val_labels = (
-#                     val_data["image"].to(device),
-#                     val_data["label"].to(device),
-#                 )
-#                 val_outputs = model(val_inputs)
-#                 val_outputs = post_trans(val_outputs)
-
-#                 # compute overall mean dice
-#                 value, not_nans = dice_metric(y_pred=val_outputs, y=val_labels)
-#                 not_nans = not_nans.mean().item()
-#                 metric_count += not_nans
-#                 metric_sum += value.mean().item() * not_nans
-#                 # compute mean dice for TC
-#                 value_tc, not_nans = dice_metric(
-#                     y_pred=val_outputs[:, 0:1], y=val_labels[:, 0:1]
-#                 )
-#                 not_nans = not_nans.item()
-#                 metric_count_tc += not_nans
-#                 metric_sum_tc += value_tc.item() * not_nans
-#                 # compute mean dice for WT
-#                 value_wt, not_nans = dice_metric(
-#                     y_pred=val_outputs[:, 1:2], y=val_labels[:, 1:2]
-#                 )
-#                 not_nans = not_nans.item()
-#                 metric_count_wt += not_nans
-#                 metric_sum_wt += value_wt.item() * not_nans
-#                 # compute mean dice for ET
-#                 value_et, not_nans = dice_metric(
-#                     y_pred=val_outputs[:, 2:3], y=val_labels[:, 2:3]
-#                 )
-#                 not_nans = not_nans.item()
-#                 metric_count_et += not_nans
-#                 metric_sum_et += value_et.item() * not_nans
-
-#             metric = metric_sum / metric_count
-#             metric_values.append(metric)
-#             metric_tc = metric_sum_tc / metric_count_tc
-#             metric_values_tc.append(metric_tc)
-#             metric_wt = metric_sum_wt / metric_count_wt
-#             metric_values_wt.append(metric_wt)
-#             metric_et = metric_sum_et / metric_count_et
-#             metric_values_et.append(metric_et)
-#             if metric > best_metric:
-#                 best_metric = metric
-#                 best_metric_epoch = epoch + 1
-#                 torch.save(
-#                     model.state_dict(),
-#                     os.path.join(root_dir, "best_metric_model.pth"),
-#                 )
-#                 print("saved new best metric model")
-#             print(
-#                 f"current epoch: {epoch + 1} current mean dice: {metric:.4f}"
-#                 f" tc: {metric_tc:.4f} wt: {metric_wt:.4f} et: {metric_et:.4f}"
-#                 f"\nbest mean dice: {best_metric:.4f}"
-#                 f" at epoch: {best_metric_epoch}"
-#             )
-
-# save_name = "./RESULTS/last.pth"
-# torch.save(model.state_dict(), save_name)
+# model definition    
+model = UNet(
+    dimensions=3,
+    in_channels=4,
+    out_channels=4,
+    channels=(16, 32, 64, 128, 256),
+    strides=(2, 2, 2, 2),
+    num_res_units=2,
+).to(device)
 
 
-# print(
-#     f"train completed, best_metric: {best_metric:.4f}" f" at epoch: {best_metric_epoch}"
-# )
+loss_function = DiceCELoss(to_onehot_y=False, sigmoid=True)
+optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4, weight_decay=1e-5)
+
+torch.cuda.empty_cache()
+
+for epoch in range(max_epochs):
+    start = time.time()
+    print(f"Epoch {epoch + 1}/{max_epochs}")
+    model.train()
+    epoch_loss = 0
+    step = 0
+    for batch_data in train_loader:
+        step += 1
+        inputs, labels = (
+            batch_data["image"].to(device),
+            batch_data["label"].to(device),
+        )
+        print(inputs.size(), labels.size())
+        optimizer.zero_grad()
+        outputs = model(inputs)
+
+        loss = loss_function(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        epoch_loss += loss.item()
+
+    epoch_loss /= step
+    epoch_loss_values.append(epoch_loss)
+    print(f"\tAverage loss: {epoch_loss:.4f}")
+
+    # evaluation
+    model.eval()
+    with torch.no_grad():
+        dice_metric = DiceMetric(include_background=True, reduction="mean")
+        post_trans = Compose(
+            [Activations(sigmoid=True), AsDiscrete(threshold_values=True)]
+        )
+        metric_sum = metric_sum_tc = metric_sum_wt = metric_sum_et = 0.0
+        metric_count = metric_count_tc = metric_count_wt = metric_count_et = 0
+        for val_data in val_loader:
+            val_inputs, val_labels = (
+                val_data["image"].to(device),
+                val_data["label"].to(device),
+            )
+            val_outputs = model(val_inputs)
+            val_outputs = post_trans(val_outputs)
+
+            # compute overall mean dice
+            value, not_nans = dice_metric(y_pred=val_outputs, y=val_labels)
+            not_nans = not_nans.mean().item()
+            metric_count += not_nans
+            metric_sum += value.mean().item() * not_nans
+            # compute mean dice for TC
+            value_tc, not_nans = dice_metric(
+                y_pred=val_outputs[:, 0:1], y=val_labels[:, 0:1]
+            )
+            not_nans = not_nans.item()
+            metric_count_tc += not_nans
+            metric_sum_tc += value_tc.item() * not_nans
+            # compute mean dice for WT
+            value_wt, not_nans = dice_metric(
+                y_pred=val_outputs[:, 1:2], y=val_labels[:, 1:2]
+            )
+            not_nans = not_nans.item()
+            metric_count_wt += not_nans
+            metric_sum_wt += value_wt.item() * not_nans
+            # compute mean dice for ET
+            value_et, not_nans = dice_metric(
+                y_pred=val_outputs[:, 2:3], y=val_labels[:, 2:3]
+            )
+            not_nans = not_nans.item()
+            metric_count_et += not_nans
+            metric_sum_et += value_et.item() * not_nans
+
+        metric = metric_sum / metric_count
+        metric_values.append(metric)
+        metric_tc = metric_sum_tc / metric_count_tc
+        metric_values_tc.append(metric_tc)
+        metric_wt = metric_sum_wt / metric_count_wt
+        metric_values_wt.append(metric_wt)
+        metric_et = metric_sum_et / metric_count_et
+        metric_values_et.append(metric_et)
+        if metric > best_metric:
+            best_metric = metric
+            best_metric_epoch = epoch + 1
+            torch.save(
+                model.state_dict(),
+                os.path.join(root_dir, "best_metric_model.pth"),
+            )
+            print("\tsaved new best metric model")
+        print(
+            f"\tMean dice: {metric:.4f}\n"
+            f"\ttc: {metric_tc:.4f} wt: {metric_wt:.4f} et: {metric_et:.4f}\n"
+            f"\tbest mean dice: {best_metric:.4f} at epoch: {best_metric_epoch}"
+        )
+
+save_name = "./RESULTS/last.pth"
+torch.save(model.state_dict(), save_name)
+
+
+print(
+    f"train completed, best_metric: {best_metric:.4f}" f" at epoch: {best_metric_epoch}"
+)
