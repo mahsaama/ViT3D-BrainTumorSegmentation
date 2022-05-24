82883dabc2ea28929d7191e22f30bf49bab525cb
mahsa.ama <mahsa.ama1391@gmail.com>
2022-05-24 21:37:16 +0430

    test

--- scripts/main.py
+++ scripts/main.py
@@ -76,7 +76,7 @@ train_transform = Compose(
     [
         # load 4 Nifti images and stack them together
         LoadImaged(keys=["image", "label"]),
-        AsChannelLastd(keys="image"),
+        AsChannelFirstd(keys="image"),
         ConvertToMultiChannelBasedOnBratsClassesd(keys="label"),
         Spacingd(
             keys=["image", "label"],
@@ -95,7 +95,7 @@ train_transform = Compose(
 val_transform = Compose(
     [
         LoadImaged(keys=["image", "label"]),
-        AsChannelLastd(keys="image"),
+        AsChannelFirstd(keys="image"),
         ConvertToMultiChannelBasedOnBratsClassesd(keys="label"),
         Spacingd(
             keys=["image", "label"],
