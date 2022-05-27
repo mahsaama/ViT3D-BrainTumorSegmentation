f60d7be5e875d79b8b1c6a6f3f99380a442c19c7
mahsa.ama <mahsa.ama1391@gmail.com>
2022-05-27 11:03:10 +0430

    y labels to 3

--- scripts/main.py
+++ scripts/main.py
@@ -164,7 +164,7 @@ print("Val Loader: ", len(val_loader))
 model = UNet(
     dimensions=3,
     in_channels=4,
-    out_channels=4,
+    out_channels=3,
     channels=(16, 32, 64, 128, 256),
     strides=(2, 2, 2, 2),
     num_res_units=2,
