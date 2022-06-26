9210481ece60144a681d7038d50a9eb411e4fc28
mahsa.ama <mahsa.ama1391@gmail.com>
2022-06-26 23:54:22 +0430

    remove extra funcs

--- utils/utils.py
+++ utils/utils.py
@@ -1,73 +1,15 @@
 from monai.transforms import MapTransform
 import numpy as np
-import torch
 import math
 import warnings
 from typing import List
 from torch.optim.lr_scheduler import LambdaLR, _LRScheduler
 from torch import nn as nn
-from torch.optim import Adam, Optimizer
+from torch.optim import Optimizer
 from torch.optim.lr_scheduler import _LRScheduler
-from torch.nn import functional as F
 
 
 __all__ = ["LinearLR", "ExponentialLR"]
-
-
-def ce_loss(true, logits, weights, ignore=255):
-    """Computes the weighted multi-class cross-entropy loss.
-    Args:
-        true: a tensor of shape [B, 1, H, W].
-        logits: a tensor of shape [B, C, H, W]. Corresponds to
-            the raw output or logits of the model.
-        weight: a tensor of shape [C,]. The weights attributed
-            to each class.
-        ignore: the class index to ignore.
-    Returns:
-        ce_loss: the weighted multi-class cross-entropy loss.
-    """
-    ce_loss = F.cross_entropy(
-        logits.float(),
-        true.long(),
-        ignore_index=ignore,
-        weight=weights,
-    )
-    return ce_loss
-
-
-def dice_loss(true, logits, eps=1e-7):
-    """Computes the Sørensen–Dice loss.
-    Note that PyTorch optimizers minimize a loss. In this
-    case, we would like to maximize the dice loss so we
-    return the negated dice loss.
-    Args:
-        true: a tensor of shape [B, 1, H, W].
-        logits: a tensor of shape [B, C, H, W]. Corresponds to
-            the raw output or logits of the model.
-        eps: added to the denominator for numerical stability.
-    Returns:
-        dice_loss: the Sørensen–Dice loss.
-    """
-    num_classes = logits.shape[1]
-    if num_classes == 1:
-        true_1_hot = torch.eye(num_classes + 1)[true.squeeze(1)]
-        true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
-        true_1_hot_f = true_1_hot[:, 0:1, :, :]
-        true_1_hot_s = true_1_hot[:, 1:2, :, :]
-        true_1_hot = torch.cat([true_1_hot_s, true_1_hot_f], dim=1)
-        pos_prob = torch.sigmoid(logits)
-        neg_prob = 1 - pos_prob
-        probas = torch.cat([pos_prob, neg_prob], dim=1)
-    else:
-        true_1_hot = torch.eye(num_classes)[true.squeeze(1)]
-        true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
-        probas = F.softmax(logits, dim=1)
-    true_1_hot = true_1_hot.type(logits.type())
-    dims = (0,) + tuple(range(2, true.ndimension()))
-    intersection = torch.sum(probas * true_1_hot, dims)
-    cardinality = torch.sum(probas + true_1_hot, dims)
-    dice_loss = (2.0 * intersection / (cardinality + eps)).mean()
-    return 1 - dice_loss
 
 
 class _LRSchedulerMONAI(_LRScheduler):
