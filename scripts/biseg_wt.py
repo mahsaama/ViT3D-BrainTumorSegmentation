8ee39244d431ca9a0e88a214499bfe5981a3db7f
mahsa.ama <mahsa.ama1391@gmail.com>
2022-07-15 18:26:54 +0430

    test

--- scripts/biseg_wt.py
+++ scripts/biseg_wt.py
@@ -250,7 +250,7 @@ for epoch in range(max_epochs):
         outputs = model(inputs)
         outputs_np = outputs.cpu().detach().numpy()
         # plt.imsave('pred_label.png', outputs_np[0, 0, :, :, 32])
-        arr = np.concatenate((label_np, outputs_np), axis=1)
+        arr = np.concatenate((label_np, outputs_np), axis=0)
         plt.imsave('true_pred.png', arr)
         print('image saved!')
 
