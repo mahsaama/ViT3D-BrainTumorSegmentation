ee7dafa5d75747dbe38a767a46cec47ba0bf05e1
mahsaama <mahsa.ama1391@gmail.com>
2022-07-26 23:55:54 +0430

    new func

--- utils/utils.py
+++ utils/utils.py
@@ -6,69 +6,83 @@ from typing import List
 from torch.optim.lr_scheduler import LambdaLR, _LRScheduler
 from torch import nn as nn
 from torch.optim import Optimizer
-from torch.optim.lr_scheduler import _LRScheduler
 import torch
-import torch.nn as nn
+from math import log
 
 
 __all__ = ["LinearLR", "ExponentialLR"]
 
 
-from math import log
-
-
 class SupervisedContrastiveLoss(nn.Module):
-    def __init__(self, temperature=0.07):
-        """
-        Implementation of the loss described in the paper Supervised Contrastive Learning :
-        https://arxiv.org/abs/2004.11362
-
-        :param temperature: int
-        """
+    def __init__(self, temperature=0.1):
         super(SupervisedContrastiveLoss, self).__init__()
         self.temperature = temperature
 
-    def forward(self, projections, targets):
-        """
-
-        :param projections: torch.Tensor, shape [batch_size, projection_dim]
-        :param targets: torch.Tensor, shape [batch_size]
-        :return: torch.Tensor, scalar
-        """
-        device = torch.device("cuda") if projections.is_cuda else torch.device("cpu")
-        print(projections.shape)
-        print(projections.T.shape)
-
-        print(torch.mm(projections, projections.T))
-        dot_product_tempered = torch.mm(projections, projections.mT) / (
-            self.temperature
+    def forward(self, feature_vectors, labels):
+        # Normalize feature vectors
+        feature_vectors_normalized = F.normalize(feature_vectors, p=2, dim=1)
+        # Compute logits
+        logits = torch.div(
+            torch.matmul(
+                feature_vectors_normalized, torch.transpose(feature_vectors_normalized, 0, 1)
+            ),
+            self.temperature,
         )
-        # Minus max for numerical stability with exponential. Same done in cross entropy. Epsilon added to avoid log(0)
-        exp_dot_tempered = (
-            torch.exp(
-                dot_product_tempered
-                - torch.max(dot_product_tempered, dim=1, keepdim=True)[0]
-            )
-            + 1e-5
-        )
+        return losses.NTXentLoss(temperature=0.07)(logits, torch.squeeze(labels))
 
-        mask_similar_class = (
-            targets.unsqueeze(1).repeat(1, targets.shape[0]) == targets
-        ).to(device)
-        mask_anchor_out = (1 - torch.eye(exp_dot_tempered.shape[0])).to(device)
-        mask_combined = mask_similar_class * mask_anchor_out
-        cardinality_per_samples = torch.sum(mask_combined, dim=1)
 
-        log_prob = -torch.log(
-            exp_dot_tempered
-            / (torch.sum(exp_dot_tempered * mask_anchor_out, dim=1, keepdim=True))
-        )
-        supervised_contrastive_loss_per_sample = (
-            torch.sum(log_prob * mask_combined, dim=1) / cardinality_per_samples
-        )
-        supervised_contrastive_loss = torch.mean(supervised_contrastive_loss_per_sample)
-
-        return supervised_contrastive_loss
+# class SupervisedContrastiveLoss(nn.Module):
+#     def __init__(self, temperature=0.07):
+#         """
+#         Implementation of the loss described in the paper Supervised Contrastive Learning :
+#         https://arxiv.org/abs/2004.11362
+#
+#         :param temperature: int
+#         """
+#         super(SupervisedContrastiveLoss, self).__init__()
+#         self.temperature = temperature
+#
+#     def forward(self, projections, targets):
+#         """
+#
+#         :param projections: torch.Tensor, shape [batch_size, projection_dim]
+#         :param targets: torch.Tensor, shape [batch_size]
+#         :return: torch.Tensor, scalar
+#         """
+#         device = torch.device("cuda") if projections.is_cuda else torch.device("cpu")
+#         print(projections.shape)
+#         print(projections.T.shape)
+#
+#         print(torch.mm(projections, projections.T))
+#         dot_product_tempered = torch.mm(projections, projections.mT) / (
+#             self.temperature
+#         )
+#         # Minus max for numerical stability with exponential. Same done in cross entropy. Epsilon added to avoid log(0)
+#         exp_dot_tempered = (
+#             torch.exp(
+#                 dot_product_tempered
+#                 - torch.max(dot_product_tempered, dim=1, keepdim=True)[0]
+#             )
+#             + 1e-5
+#         )
+#
+#         mask_similar_class = (
+#             targets.unsqueeze(1).repeat(1, targets.shape[0]) == targets
+#         ).to(device)
+#         mask_anchor_out = (1 - torch.eye(exp_dot_tempered.shape[0])).to(device)
+#         mask_combined = mask_similar_class * mask_anchor_out
+#         cardinality_per_samples = torch.sum(mask_combined, dim=1)
+#
+#         log_prob = -torch.log(
+#             exp_dot_tempered
+#             / (torch.sum(exp_dot_tempered * mask_anchor_out, dim=1, keepdim=True))
+#         )
+#         supervised_contrastive_loss_per_sample = (
+#             torch.sum(log_prob * mask_combined, dim=1) / cardinality_per_samples
+#         )
+#         supervised_contrastive_loss = torch.mean(supervised_contrastive_loss_per_sample)
+#
+#         return supervised_contrastive_loss
 
 
 class BinaryLabel_WT(MapTransform):
