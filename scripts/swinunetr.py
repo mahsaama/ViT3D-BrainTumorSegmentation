f7cfecaea3e637935e423a77bd8caf463e3fa9c5
mahsaama <mahsa.ama1391@gmail.com>
2022-07-29 18:07:47 +0430

    fix

--- scripts/swinunetr.py
+++ scripts/swinunetr.py
@@ -239,7 +239,7 @@ for epoch in range(max_epochs):
             batch_data["images"].to(device),
             batch_data["label"].to(device),
         )
-        print(inputs.size(), labels.size())
+        # print(inputs.size(), labels.size())
         # print(torch.unique(labels))
         optimizer.zero_grad()
         outputs = model(inputs)
