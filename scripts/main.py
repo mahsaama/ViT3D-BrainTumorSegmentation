e7f4c5ac57cdf893f4d29d263636a0a34db5fed4
mahsaama <mahsa.ama1391@gmail.com>
2022-12-12 11:16:47 +0330

    kaggle test

--- scripts/main.py
+++ scripts/main.py
@@ -89,7 +89,8 @@ metric_values_wt = []
 metric_values_et = []
 
 if ds == "2020":
-    data_dir = "../Dataset_BRATS_2020/Training/"
+    data_dir = "/kaggle/input/brats20-dataset-training-validation/BraTS2020_TrainingData/"
+    # data_dir = "../Dataset_BRATS_2020/Training/"
     t1_list = sorted(glob.glob(data_dir + "*/*t1.nii.gz"))
     t2_list = sorted(glob.glob(data_dir + "*/*t2.nii.gz"))
     t1ce_list = sorted(glob.glob(data_dir + "*/*t1ce.nii.gz"))
