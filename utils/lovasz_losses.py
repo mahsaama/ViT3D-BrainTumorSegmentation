980a0af28a9ffdb0d363a25890f86bc8dfb4d476
mahsaama <mahsa.ama1391@gmail.com>
2022-12-18 19:47:07 +0330

    d

--- utils/lovasz_losses.py
+++ utils/lovasz_losses.py
@@ -194,9 +194,9 @@ def lovasz_softmax_flat(probas, labels, classes='present'):
             class_pred = probas[:, c]
         errors = (Variable(fg) - class_pred).abs()
         errors_sorted, perm = torch.sort(errors, 0, descending=True)
-        print(fg, fg.size())
-        print(perm, perm.size())
-        # perm = perm.data
+        print(fg, fg.size(), fg.dtype)
+        print(perm, perm.size(), perm.dtype)
+        perm = perm.data
         fg_sorted = fg[perm]
         # fg_sorted = torch.index_select(fg, 0, perm)
         losses.append(torch.dot(errors_sorted, Variable(lovasz_grad(fg_sorted))))
