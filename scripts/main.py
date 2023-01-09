42a92517e749a6ccbcc00babb650f655ebaf899f
mahsaama <mahsa.ama1391@gmail.com>
2023-01-09 16:03:16 +0330

    gamma 0 for focal loss

--- scripts/main.py
+++ scripts/main.py
@@ -315,7 +315,7 @@ print("Total parameters count", pytorch_total_params)
 # loss_function = DiceLoss(to_onehot_y=False, sigmoid=True)
 # loss_function = DiceLoss(to_onehot_y=False, sigmoid=True, squared_pred=True, smooth_nr=0.0, smooth_dr=1e-6)
 # loss_function = lovasz_softmax
-loss_function = FocalLoss()
+loss_function = FocalLoss(gamma=0)
 
 optimizer = torch.optim.AdamW(model.parameters(), lr=lr, weight_decay=1e-5)
 # optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=0.99, nesterov=True, weight_decay=1e-5)
